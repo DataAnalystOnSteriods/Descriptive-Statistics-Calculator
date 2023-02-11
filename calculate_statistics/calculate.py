6c441e8d400f097d96454ea6a95c9a14b4613001
kristianroger0101 <kristianrogeragdeppa@gmail.com>
2023-02-11 23:07:00 +0800

    added a new button in which the user can choose whether it is a sample or a population. changed the color theme and added some new calculations such as skew and size.

--- calculate_statistics/calculate.py
+++ calculate_statistics/calculate.py
@@ -1,13 +1,20 @@
 import tkinter as tk
+import customtkinter as ctk
 from tkinter.messagebox import showwarning
 from numpy import sum, percentile, mean, array
 import statistics as stats
-import customtkinter
+from scipy.stats import skew
 import matplotlib.pyplot as plt
+import matplotlib.font_manager
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import seaborn as sns
+from seaborn import kdeplot
 
+# global variables
 INPUT_TEXTBOX = None
+DATAGROUP_OPTION = None
+
+DEFAULT_FONTSIZE = 15
+DEFAULT_FONTSTYLE = "Calibri Light"
 
 plt.rcParams.update({
     "lines.color": "white",
@@ -25,150 +32,119 @@ plt.rcParams.update({
     "savefig.edgecolor": "#242424"})
 
 
-def calculate_and_show_stats(window) -> None:
-    SUM_NUMBER = tk.IntVar()
-    MINIMUM_NUMBER = tk.IntVar()
-    MAXIMUM_NUMBER = tk.IntVar()
-    AVERAGE_NUMBER = tk.IntVar()
-    MEDIAN_NUMBER = tk.IntVar()
-    MODE_NUMBER = tk.IntVar()
-    RANGE_NUMBER = tk.IntVar()
-    MIDRANGE_NUMBER = tk.IntVar()
-    POPULATION_STANDARD_DEVIATION = tk.IntVar()
-    SAMPLE_STANDARD_DEVIATION = tk.IntVar()
-    POPULATION_VARIANCE = tk.IntVar()
-    SAMPLE_VARIANCE = tk.IntVar()
-    QUARTILE_25 = tk.IntVar()
-    QUARTILE_50 = tk.IntVar()
-    QUARTILE_75 = tk.IntVar()
-    INTERQUARTILE_RANGE = tk.IntVar()
+class CalculateAndShow:
 
-    OUTPUT_FONTSIZE = 15
-    DEFAULT_FONTSTYLE = "Courier New"
+    def __init__(self, window: ctk.CTk, result_var: ctk.IntVar, x: int, y: int):
+        self.window = window
+        self.result_var = result_var
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def calculate_stats(master: ctk.CTk, **tkvariables_: ctk.IntVar) -> None:
+        global INPUT_TEXTBOX
+        RETRIEVED_USER_INPUT = INPUT_TEXTBOX.get("1.0", 'end-1c')
+        LIST_OF_NUMBERS = []
+
+        for numbers in RETRIEVED_USER_INPUT.split(','):
+            numbers = numbers.strip()
+            if numbers.lstrip('-').isnumeric():
+                LIST_OF_NUMBERS.append(int(numbers))
+
+        if len(LIST_OF_NUMBERS) > 2_500:
+            WARNING_TITLE = "Warning!"
+            WARNING_MESSEGE = "Please, Enter less than 2,500 values!"
+            showwarning(WARNING_TITLE, WARNING_MESSEGE)
+        elif len(LIST_OF_NUMBERS) <= 1:
+            WARNING_TITLE = "Warning!"
+            WARNING_MESSEGE = "Please, Enter more than 1 values!"
+            showwarning(WARNING_TITLE, WARNING_MESSEGE)
+        else:
+            tkvariables_['SUMM'].set(sum(LIST_OF_NUMBERS))
+            tkvariables_['MINN'].set(min(LIST_OF_NUMBERS))
+            tkvariables_['MAXX'].set(max(LIST_OF_NUMBERS))
+            tkvariables_['AVG'].set(round(mean(LIST_OF_NUMBERS), 3))
+            tkvariables_['MEDIAN'].set(stats.median(LIST_OF_NUMBERS))
+            tkvariables_['MODE'].set(stats.mode(LIST_OF_NUMBERS))
+            tkvariables_['RANGE'].set(tkvariables_['MAXX'].get() - tkvariables_['MINN'].get())
+            tkvariables_['MIDRANGE'].set(tkvariables_['RANGE'].get() / 2)
+
+            global DATAGROUP_OPTION
+            if DATAGROUP_OPTION == 'Population':
+                tkvariables_['STD'].set((round(stats.pstdev(LIST_OF_NUMBERS), 3)))
+                tkvariables_['VAR'].set((round(stats.pvariance(LIST_OF_NUMBERS), 3)))
+            elif DATAGROUP_OPTION == 'Sample':
+                tkvariables_['STD'].set((round(stats.stdev(LIST_OF_NUMBERS), 3)))
+                tkvariables_['VAR'].set((round(stats.variance(LIST_OF_NUMBERS)), 3))
+
+            tkvariables_['SKEW'].set((round(skew(LIST_OF_NUMBERS, axis=0, bias=True), 3)))
+            tkvariables_['SIZE_'].set(len(LIST_OF_NUMBERS))
+
+            PERCENTILE_25TH = 25
+            PERCENTILE_50TH = 50
+            PERCENTILE_75TH = 75
+            Q25_, Q50_, Q75_ = percentile(LIST_OF_NUMBERS, [PERCENTILE_25TH, PERCENTILE_50TH, PERCENTILE_75TH])
+
+            tkvariables_['Q25'].set(Q25_)
+            tkvariables_['Q50'].set(Q50_)
+            tkvariables_['Q75'].set(Q75_)
+            tkvariables_['IQR'].set(tkvariables_['Q75'].get() - tkvariables_['Q25'].get())
+
+            global DEFAULT_FONTSIZE, DEFAULT_FONTSTYLE
+            PLOT_WIDTH = 5.5
+            PLOT_HEIGHT = 3.5
+            PLOT_TITLE = 'Kernel Density Plot'
+            PLOT_FONTSIZE = 12
+
+            FIGURE, AXES = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HEIGHT))
+
+            plt.title(PLOT_TITLE, fontsize=PLOT_FONTSIZE, font=DEFAULT_FONTSTYLE)
+
+            KDE_PLOT = FigureCanvasTkAgg(FIGURE, master)
+            KDE_PLOT.get_tk_widget().place(x=390, y=205)
+
+            SEABORN_PLOT = kdeplot(array(LIST_OF_NUMBERS), color='#2FA572')
+            SEABORN_PLOT.set(ylabel=None)
+            plt.close('all')
+
+    def show_calculations(self):
+        global DEFAULT_FONTSIZE, DEFAULT_FONTSTYLE
+
+        RESULT_OUTPUT = ctk.CTkLabel(self.window, textvariable=self.result_var)
+        RESULT_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, DEFAULT_FONTSIZE))
+        RESULT_OUTPUT.place(x=self.x, y=self.y)
+
+
+def textbox_input(frame: ctk.CTkFrame) -> None:
     global INPUT_TEXTBOX
-
-    RETRIEVED_USER_INPUT = INPUT_TEXTBOX.get("1.0", 'end-1c')
-    LIST_OF_NUMBERS = []
-
-    for numbers in RETRIEVED_USER_INPUT.split(','):
-        numbers = numbers.strip()
-        if numbers.lstrip('-').isnumeric():
-            LIST_OF_NUMBERS.append(int(numbers))
-
-    if len(LIST_OF_NUMBERS) > 2_500:
-        WARNING_TITLE = "Warning!"
-        WARNING_MESSEGE = "Please, Enter less than 2,500 values!"
-        showwarning(WARNING_TITLE, WARNING_MESSEGE)
-    elif len(LIST_OF_NUMBERS) <= 1:
-        WARNING_TITLE = "Warning!"
-        WARNING_MESSEGE = "Please, Enter more than 1 values!"
-        showwarning(WARNING_TITLE, WARNING_MESSEGE)
-    else:
-        SUM_NUMBER.set(sum(LIST_OF_NUMBERS))
-        SUMMATION_OUTPUT = customtkinter.CTkLabel(window, textvariable=SUM_NUMBER)
-        SUMMATION_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        SUMMATION_OUTPUT.place(x=555, y=42)
-
-        MINIMUM_NUMBER.set(min(LIST_OF_NUMBERS))
-        MINIMUM_NUMBER_OUTPUT = customtkinter.CTkLabel(window, textvariable=MINIMUM_NUMBER)
-        MINIMUM_NUMBER_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MINIMUM_NUMBER_OUTPUT.place(x=515, y=70)
-
-        MAXIMUM_NUMBER.set(max(LIST_OF_NUMBERS))
-        MAXIMUM_NUMBER_OUTPUT = customtkinter.CTkLabel(window, textvariable=MAXIMUM_NUMBER)
-        MAXIMUM_NUMBER_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MAXIMUM_NUMBER_OUTPUT.place(x=515, y=100)
-
-        AVERAGE_NUMBER.set(round(mean(LIST_OF_NUMBERS), 3))
-        MEAN_LABEL_OUTPUT = customtkinter.CTkLabel(window, textvariable=AVERAGE_NUMBER)
-        MEAN_LABEL_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MEAN_LABEL_OUTPUT.place(x=494, y=130)
-
-        MEDIAN_NUMBER.set(stats.median(LIST_OF_NUMBERS))
-        MEDIAN_LABEL_OUTPUT = customtkinter.CTkLabel(window, textvariable=MEDIAN_NUMBER)
-        MEDIAN_LABEL_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MEDIAN_LABEL_OUTPUT.place(x=505, y=160)
-
-        MODE_NUMBER.set(stats.mode(LIST_OF_NUMBERS))
-        MODE_NUMBER_OUTPUT = customtkinter.CTkLabel(window, textvariable=MODE_NUMBER)
-        MODE_NUMBER_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MODE_NUMBER_OUTPUT.place(x=140, y=210)
-
-        RANGE_NUMBER.set(MAXIMUM_NUMBER.get() - MINIMUM_NUMBER.get())
-        RANGE_NUMBER_OUTPUT = customtkinter.CTkLabel(window, textvariable=RANGE_NUMBER)
-        RANGE_NUMBER_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        RANGE_NUMBER_OUTPUT.place(x=144, y=240)
-
-        MIDRANGE_NUMBER.set(RANGE_NUMBER.get() / 2)
-        MIDRANGE_NUMBER_OUTPUT = customtkinter.CTkLabel(window, textvariable=MIDRANGE_NUMBER)
-        MIDRANGE_NUMBER_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        MIDRANGE_NUMBER_OUTPUT.place(x=170, y=270)
-
-        POPULATION_STANDARD_DEVIATION.set((round(stats.pstdev(LIST_OF_NUMBERS), 3)))
-        POPULATION_STANDARD_DEVIATION_OUTPUT = customtkinter.CTkLabel(window,
-                                                                      textvariable=POPULATION_STANDARD_DEVIATION)
-        POPULATION_STANDARD_DEVIATION_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        POPULATION_STANDARD_DEVIATION_OUTPUT.place(x=250, y=300)
-
-        SAMPLE_STANDARD_DEVIATION.set((round(stats.stdev(LIST_OF_NUMBERS), 3)))
-        SAMPLE_STANDARD_DEVIATION_OUTPUT = customtkinter.CTkLabel(window, textvariable=SAMPLE_STANDARD_DEVIATION)
-        SAMPLE_STANDARD_DEVIATION_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        SAMPLE_STANDARD_DEVIATION_OUTPUT.place(x=205, y=330)
-
-        POPULATION_VARIANCE.set((round(stats.pvariance(LIST_OF_NUMBERS), 3)))
-        POPULATION_VARIANCE_OUTPUT = customtkinter.CTkLabel(window, textvariable=POPULATION_VARIANCE)
-        POPULATION_VARIANCE_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        POPULATION_VARIANCE_OUTPUT.place(x=325, y=360)
-
-        SAMPLE_VARIANCE.set((round(stats.variance(LIST_OF_NUMBERS), 3)))
-        SAMPLE_VARIANCE_OUTPUT = customtkinter.CTkLabel(master=window, textvariable=SAMPLE_VARIANCE)
-        SAMPLE_VARIANCE_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        SAMPLE_VARIANCE_OUTPUT.place(x=275, y=390)
-
-        PERCENTILE_25TH = 25
-        PERCENTILE_50TH = 50
-        PERCENTILE_75TH = 75
-
-        Q25, Q50, Q75 = percentile(LIST_OF_NUMBERS, [PERCENTILE_25TH, PERCENTILE_50TH, PERCENTILE_75TH])
-
-        QUARTILE_25.set(Q25)
-        QUARTILE_25_OUTPUT = customtkinter.CTkLabel(window, textvariable=QUARTILE_25)
-        QUARTILE_25_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        QUARTILE_25_OUTPUT.place(x=175, y=445)
-
-        QUARTILE_50.set(Q50)
-        QUARTILE_50_OUTPUT = customtkinter.CTkLabel(window, textvariable=QUARTILE_50)
-        QUARTILE_50_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        QUARTILE_50_OUTPUT.place(x=175, y=470)
-
-        QUARTILE_75.set(Q75)
-        QUARTILE_75_OUTPUT = customtkinter.CTkLabel(window, textvariable=QUARTILE_75)
-        QUARTILE_75_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        QUARTILE_75_OUTPUT.place(x=175, y=500)
-
-        INTERQUARTILE_RANGE.set(QUARTILE_75.get() - QUARTILE_25.get())
-        INTERQUARTILE_RANGE_OUTPUT = customtkinter.CTkLabel(window, textvariable=INTERQUARTILE_RANGE)
-        INTERQUARTILE_RANGE_OUTPUT.configure(font=(DEFAULT_FONTSTYLE, OUTPUT_FONTSIZE, 'underline'))
-        INTERQUARTILE_RANGE_OUTPUT.place(x=305, y=525)
-
-        PLOT_WIDTH = 5.5
-        PLOT_HEIGHT = 3.5
-        PLOT_TITLE = 'KDE Plot'
-        PLOT_XLABEL = 'Density'
-        PLOT_FONTSIZE = 12
-
-        FIGURE, AXES = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HEIGHT))
-
-        plt.title(PLOT_TITLE, fontsize=PLOT_FONTSIZE, font=DEFAULT_FONTSTYLE)
-        plt.ylabel(PLOT_XLABEL, fontsize=PLOT_FONTSIZE, font=DEFAULT_FONTSTYLE)
-
-        HISTOGRAM = FigureCanvasTkAgg(FIGURE, window)
-        HISTOGRAM.get_tk_widget().place(x=390, y=205)
-
-        sns.kdeplot(array(LIST_OF_NUMBERS))
-
-
-def textbox_input(FRAME) -> None:
-    global INPUT_TEXTBOX
-    INPUT_TEXTBOX = customtkinter.CTkTextbox(FRAME, height=100, width=300)
+    INPUT_TEXTBOX = ctk.CTkTextbox(frame, height=100, width=300, undo=True, autoseparators=True, maxundo=-1)
     INPUT_TEXTBOX.place(x=20, y=15)
+
+
+def option_setter(variable: ctk.StringVar) -> None:
+    global DATAGROUP_OPTION
+    DATAGROUP_OPTION = variable.get()
+
+
+def population_button(frame: ctk.CTkFrame, population_option: ctk.StringVar) -> None:
+    global DEFAULT_FONTSIZE, DEFAULT_FONTSTYLE
+    CHANGE_DATAGROUP_OPTION = 'Population'
+    RADIOBUTTON_TITLE = 'Population'
+    POPULATION_RADIOBUTTON = ctk.CTkRadioButton(master=frame, variable=population_option,
+                                                value=CHANGE_DATAGROUP_OPTION,
+                                                text=RADIOBUTTON_TITLE, radiobutton_height=15, radiobutton_width=15,
+                                                command=lambda: option_setter(
+                                                    population_option), font=(DEFAULT_FONTSTYLE, DEFAULT_FONTSIZE))
+    POPULATION_RADIOBUTTON.place(x=17, y=125)
+
+
+def sample_button(frame: ctk.CTkFrame, sample_option: ctk.StringVar) -> None:
+    global DEFAULT_FONTSIZE, DEFAULT_FONTSTYLE
+    CHANGE_DATAGROUP_OPTION = 'Sample'
+    RADIOBUTTON_TITLE = 'Sample'
+    SAMPLE_RADIOBUTTON = ctk.CTkRadioButton(master=frame,
+                                            variable=sample_option, value=CHANGE_DATAGROUP_OPTION,
+                                            text=RADIOBUTTON_TITLE, radiobutton_height=15, radiobutton_width=15,
+                                            command=lambda: option_setter(
+                                                sample_option), font=(DEFAULT_FONTSTYLE, DEFAULT_FONTSIZE))
+    SAMPLE_RADIOBUTTON.place(x=17, y=145)
--- calculate_statistics/main.py
+++ calculate_statistics/main.py
@@ -1,30 +1,53 @@
-import tkinter
 import tkinter as tk
 import customtkinter as ctk
+import matplotlib.font_manager
 import calculate
+from PIL import ImageTk, Image
 import sys
 
 
 def main() -> None:
-    WINDOW = ctk.CTk()
+    MAIN_WINDOW = ctk.CTk()
 
     ROOT_GEOMETRY = "900x570"
-    WINDOW.geometry(ROOT_GEOMETRY)
+    MAIN_WINDOW.geometry(ROOT_GEOMETRY)
 
     APP_TITLE = "Descriptive Statistics Calculator"
-    WINDOW.title(APP_TITLE)
+    MAIN_WINDOW.title(APP_TITLE)
 
     MAX_WINDOW_WIDTH = 900
     MAX_WINDOW_HEIGHT = 570
-    WINDOW.maxsize(MAX_WINDOW_WIDTH, MAX_WINDOW_HEIGHT)
+    MAIN_WINDOW.minsize(MAX_WINDOW_WIDTH, MAX_WINDOW_HEIGHT)
+    MAIN_WINDOW.maxsize(MAX_WINDOW_WIDTH, MAX_WINDOW_HEIGHT)
 
-    DEFAULT_COLOR_THEME = 'blue'
+    DEFAULT_COLOR_THEME = 'green'
     ctk.set_default_color_theme(DEFAULT_COLOR_THEME)
 
-    BOX_AND_BUTTON_FRAME = ctk.CTkFrame(master=WINDOW, height=170, width=350)
+    SUM_NUMBER = ctk.IntVar()
+    MINIMUM_NUMBER = ctk.IntVar()
+    MAXIMUM_NUMBER = ctk.IntVar()
+    AVERAGE_NUMBER = ctk.IntVar()
+    MEDIAN_NUMBER = ctk.IntVar()
+    MODE_NUMBER = ctk.IntVar()
+    RANGE_NUMBER = ctk.IntVar()
+    MIDRANGE_NUMBER = ctk.IntVar()
+    STANDARD_DEVIATION = ctk.IntVar()
+    VARIANCE = ctk.IntVar()
+    SKEWNESS = ctk.IntVar()
+    SIZE = ctk.IntVar()
+    QUARTILE_25 = ctk.IntVar()
+    QUARTILE_50 = ctk.IntVar()
+    QUARTILE_75 = ctk.IntVar()
+    INTERQUARTILE_RANGE = ctk.IntVar()
+
+    BOX_AND_BUTTON_FRAME = ctk.CTkFrame(master=MAIN_WINDOW, height=170, width=350)
     BOX_AND_BUTTON_FRAME.place(rely=0.07, relx=0.05)
 
-    INPUT_TEXTBOX = calculate.INPUT_TEXTBOX
+    DEFAULT_FONTSTYLE = "Calibri Light"
+    DEFAULT_FONTSIZE = 20
+
+    DEFAULT_OPTION = 'Sample'
+    SAMPLE_OR_POPULATION_OPTION = tk.StringVar(value=DEFAULT_OPTION)
 
     class ShowCalculationLabels:
         def __init__(self, x: int, y: int, LABEL_TITLE: str):
@@ -33,41 +56,70 @@ def main() -> None:
             self.LABEL_TITLE = LABEL_TITLE
 
         def window_labels(self) -> None:
-            FONT_STYLE = "Courier New"
-            FONT_SIZE = 20
-            CALCULATION_LABELS = ctk.CTkLabel(WINDOW, text=self.LABEL_TITLE, font=(FONT_STYLE, FONT_SIZE))
+            CALCULATION_LABELS = ctk.CTkLabel(MAIN_WINDOW, text=self.LABEL_TITLE,
+                                              font=(DEFAULT_FONTSTYLE, DEFAULT_FONTSIZE))
             CALCULATION_LABELS.place(x=self.x, y=self.y)
 
     class ShowButton:
 
-        def show_results(BUTTON_HEIGHT, BUTTON_WIDTH, text_) -> None:
-            CALCULATION_BUTTON = ctk.CTkButton(master=BOX_AND_BUTTON_FRAME, height=BUTTON_HEIGHT, width=BUTTON_WIDTH, text=text_,
-                                                         command=lambda: calculate.calculate_and_show_stats(WINDOW))
+        @staticmethod
+        def show_results(results_height: int, results_width: int, button_text: str) -> None:
+            CALCULATION_BUTTON = ctk.CTkButton(master=BOX_AND_BUTTON_FRAME, height=results_height, width=results_width,
+                                               text=button_text, command=lambda: [
+                    calculate.CalculateAndShow.calculate_stats(MAIN_WINDOW, SUMM=SUM_NUMBER, MINN=MINIMUM_NUMBER,
+                                                               MAXX=MAXIMUM_NUMBER, AVG=AVERAGE_NUMBER,
+                                                               MEDIAN=MEDIAN_NUMBER, MODE=MODE_NUMBER,
+                                                               RANGE=RANGE_NUMBER, MIDRANGE=MIDRANGE_NUMBER,
+                                                               STD=STANDARD_DEVIATION, VAR=VARIANCE, SKEW=SKEWNESS,
+                                                               SIZE_=SIZE, Q25=QUARTILE_25,
+                                                               Q50=QUARTILE_50, Q75=QUARTILE_75,
+                                                               IQR=INTERQUARTILE_RANGE)], font=(DEFAULT_FONTSTYLE, 15))
             CALCULATION_BUTTON.place(x=200, y=125)
 
-
     calculate.textbox_input(BOX_AND_BUTTON_FRAME)
-    ShowButton.show_results(32, 120, 'Calculate')
 
-    FRONT_TITLE = ShowCalculationLabels(240, 0, 'Descriptive Statistics Calculator')
+    FRONT_TITLE = ShowCalculationLabels(300, 0, 'Descriptive Statistics Calculator')
 
-    SUMMATION_LABEL = ShowCalculationLabels(x=405, y=42, LABEL_TITLE='Summation Σ:')
+    calculate.option_setter(SAMPLE_OR_POPULATION_OPTION)
+    calculate.population_button(BOX_AND_BUTTON_FRAME, SAMPLE_OR_POPULATION_OPTION)
+    calculate.sample_button(BOX_AND_BUTTON_FRAME, SAMPLE_OR_POPULATION_OPTION)
+
+    SUMMATION_LABEL = ShowCalculationLabels(x=405, y=42, LABEL_TITLE='Summation :')
     MIN_NUMBER_LABEL = ShowCalculationLabels(x=405, y=70, LABEL_TITLE='Minimum :')
     MAX_NUMBER_LABEL = ShowCalculationLabels(x=405, y=100, LABEL_TITLE='Maximum :')
-    AVERAGE_LABEL = ShowCalculationLabels(x=405, y=130, LABEL_TITLE='Mean μ:')
+    AVERAGE_LABEL = ShowCalculationLabels(x=405, y=130, LABEL_TITLE='Mean μ :')
     MEDIAN_LABEL = ShowCalculationLabels(x=405, y=160, LABEL_TITLE='Median :')
     MODE_LABEL = ShowCalculationLabels(x=45, y=210, LABEL_TITLE='Mode :')
     RANGE_LABEL = ShowCalculationLabels(x=45, y=240, LABEL_TITLE='Range :')
     MIDRANGE_LABEL = ShowCalculationLabels(x=45, y=270, LABEL_TITLE='Midrange :')
-    POPULATION_STANDARD_DEV_LABEL = ShowCalculationLabels(x=45, y=300, LABEL_TITLE='Population STD σ:')
-    SAMPLE_STANDARD_DEV_LABEL = ShowCalculationLabels(x=45, y=330, LABEL_TITLE='Sample STD σ:')
-    POPULATION_VARIANCE_LABEL = ShowCalculationLabels(x=45, y=360, LABEL_TITLE='Population Variance σ2:')
-    SAMPLE_VARIANCE_LABEL = ShowCalculationLabels(x=45, y=390, LABEL_TITLE='Sample Variance σ2:')
+    VARIANCE_LABEL = ShowCalculationLabels(x=45, y=300, LABEL_TITLE='Variance :')
+    STANDARD_DEV_LABEL = ShowCalculationLabels(x=45, y=330, LABEL_TITLE='Standard Dev :')
+    SKEW_LABEL = ShowCalculationLabels(x=45, y=360, LABEL_TITLE='Skewness :')
+    SIZE_LABEL = ShowCalculationLabels(x=45, y=390, LABEL_TITLE='Size :')
     QUARTILE_TITLE = ShowCalculationLabels(x=45, y=420, LABEL_TITLE='Quartiles')
     QUARTILE_25_LABEL = ShowCalculationLabels(x=100, y=445, LABEL_TITLE='Q25 :')
     QUARTILE_50_LABEL = ShowCalculationLabels(x=100, y=470, LABEL_TITLE='Q50 :')
     QUARTILE_75_LABEL = ShowCalculationLabels(x=100, y=500, LABEL_TITLE='Q75 :')
     INTERQUARTILE_RANGE_LABEL = ShowCalculationLabels(x=45, y=525, LABEL_TITLE='Interquartile Range :')
+
+    SUMMATION_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, SUM_NUMBER, 515, 42)
+    MINIMUM_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, MINIMUM_NUMBER, 500, 70)
+    MAXIMUM_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, MAXIMUM_NUMBER, 500, 100)
+    AVERAGE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, AVERAGE_NUMBER, 485, 130)
+    MEDIAN_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, MEDIAN_NUMBER, 485, 160)
+    MODE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, MODE_NUMBER, 110, 210)
+    RANGE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, RANGE_NUMBER, 115, 240)
+    MIDRANGE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, MIDRANGE_NUMBER, 140, 270)
+    VARIANCE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, VARIANCE, 135, 300)
+    STANDARD_DEVIATION_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, STANDARD_DEVIATION, 170, 330)
+    SKEWNESS_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, SKEWNESS, 140, 360)
+    SIZE_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, SIZE, 95, 390)
+    QUARTILE_25_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, QUARTILE_25, 150, 445)
+    QUARTILE_50_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, QUARTILE_50, 150, 470)
+    QUARTILE_75_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, QUARTILE_75, 150, 500)
+    IQR_OUTPUT = calculate.CalculateAndShow(MAIN_WINDOW, INTERQUARTILE_RANGE, 220, 525)
+
+    ShowButton.show_results(32, 120, 'Calculate')
 
     FRONT_TITLE.window_labels()
     SUMMATION_LABEL.window_labels()
@@ -78,17 +130,34 @@ def main() -> None:
     MODE_LABEL.window_labels()
     RANGE_LABEL.window_labels()
     MIDRANGE_LABEL.window_labels()
-    POPULATION_STANDARD_DEV_LABEL.window_labels()
-    SAMPLE_STANDARD_DEV_LABEL.window_labels()
-    POPULATION_VARIANCE_LABEL.window_labels()
-    SAMPLE_VARIANCE_LABEL.window_labels()
+    VARIANCE_LABEL.window_labels()
+    STANDARD_DEV_LABEL.window_labels()
+    SKEW_LABEL.window_labels()
+    SIZE_LABEL.window_labels()
     QUARTILE_TITLE.window_labels()
     QUARTILE_25_LABEL.window_labels()
     QUARTILE_50_LABEL.window_labels()
     QUARTILE_75_LABEL.window_labels()
     INTERQUARTILE_RANGE_LABEL.window_labels()
 
-    WINDOW.mainloop()
+    SUMMATION_OUTPUT.show_calculations()
+    MINIMUM_OUTPUT.show_calculations()
+    MAXIMUM_OUTPUT.show_calculations()
+    AVERAGE_OUTPUT.show_calculations()
+    MEDIAN_OUTPUT.show_calculations()
+    MODE_OUTPUT.show_calculations()
+    RANGE_OUTPUT.show_calculations()
+    MIDRANGE_OUTPUT.show_calculations()
+    STANDARD_DEVIATION_OUTPUT.show_calculations()
+    VARIANCE_OUTPUT.show_calculations()
+    SKEWNESS_OUTPUT.show_calculations()
+    SIZE_OUTPUT.show_calculations()
+    QUARTILE_25_OUTPUT.show_calculations()
+    QUARTILE_50_OUTPUT.show_calculations()
+    QUARTILE_75_OUTPUT.show_calculations()
+    IQR_OUTPUT.show_calculations()
+
+    MAIN_WINDOW.mainloop()
 
 
 if __name__ == '__main__':
